cb16579f7ac11f39c0af4f8c61e8b3aa62c3e5e8
Pingfan Hu <pingfan0727@gmail.com>
2025-09-29 16:09:28 -0400

    initial push

---
+++ README.md
@@ -0,0 +1,3 @@
+The `live_polling` surveydown template.
+
+This template requires actual database connection to work, so it doesn't have an online demo.
---
+++ app.R
@@ -0,0 +1,62 @@
+# Package setup ---------------------------------------------------------------
+
+# Install required packages:
+# install.packages("pak")
+# pak::pak(c(
+#   'surveydown-dev/surveydown', # Development version from GitHub
+#   'ggplot2',
+#   'dplyr'
+# ))
+
+# Load packages
+library(surveydown)
+library(dplyr)
+library(ggplot2)
+
+# Database setup --------------------------------------------------------------
+#
+# Details at: https://surveydown.org/docs/storing-data
+#
+# surveydown stores data on any PostgreSQL database. We recommend
+# https://supabase.com/ for a free and easy to use service.
+#
+# Once you have your database ready, run the following function to store your
+# database configuration parameters in a local .env file:
+
+# sd_db_config()
+
+# For this template, we recommend setting ignore = FALSE (or leaving it blank)
+# in the sd_db_connect() function, since live-polling requires a database to
+# work.
+
+db <- sd_db_connect(ignore = FALSE)
+
+# UI setup --------------------------------------------------------------------
+
+ui <- sd_ui()
+
+# Server setup ----------------------------------------------------------------
+
+server <- function(input, output, session) {
+    # Refresh data every 5 seconds
+    data <- sd_get_data(db, refresh_interval = 5)
+
+    # Render the plot
+    output$penguin_plot <- renderPlot({
+        data() |> # Note the () here, as this is a reactive expression
+            count(penguins) |>
+            mutate(
+                penguins = ifelse(penguins == '', 'No response', penguins)
+            ) |>
+            ggplot() +
+            geom_col(aes(x = n, y = reorder(penguins, n)), width = 0.7) +
+            theme_minimal() +
+            labs(x = "Count", y = "Penguin Type", title = "Penguin Count")
+    })
+
+    # Run surveydown server and define database
+    sd_server(db = db)
+}
+
+# Launch the app
+shiny::shinyApp(ui = ui, server = server)
